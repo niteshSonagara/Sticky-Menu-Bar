733852aa0e5d52da5b4ca51491953f7709d2abc0
Nitesh Sonagara <sonagaranitesh11@gmail.com>
2022-06-28 13:30:14 +0530

    Sticky Menu Bar

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll',function(){
+
+    let navbar = document.getElementById('menu-bar');
+
+    if(window.pageYOffset >= 213 ){
+        navbar.classList.add('sticky')
+
+    }else{
+        navbar.classList.remove('sticky')
+    }
+
+});
